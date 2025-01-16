d3b6015d8fbee18d71cc4cd0941de6c0eff51fb0
Micah Woodard <micah.woodard@alleninstitute.org>
2025-01-16 14:15:55 -0800

    changing naming conventions

--- src/aind_behavior_dynamic_foraging/DataSchemas/task_logic.py
+++ src/aind_behavior_dynamic_foraging/DataSchemas/task_logic.py
@@ -6,18 +6,19 @@ from typing import Literal
 from aind_behavior_services.task_logic import AindBehaviorTaskLogicModel, TaskParameters
 from pydantic import Field
 
-__version__ = "0.1.0"
+__version__ = "0.0.0"
 
 
 class AdvancedBlockMode(str, Enum):
-    ''' Modes for advanced block '''
+    """ Modes for advanced block """
+
     OFF = "off"
     NOW = "now"
     ONCE = "once"
 
 
 class AutoWaterMode(str, Enum):
-    ''' Modes for auto water '''
+    """Modes for auto water """
     NATURAL = "Natural"
     BOTH = "Both"
     HIGH_PRO = "High pro"
@@ -25,60 +26,60 @@ class AutoWaterMode(str, Enum):
 class AindDynamicForagingTaskParameters(TaskParameters):
     # --- Critical training parameters ---
     # Reward probability
-    BaseRewardSum: float = Field(..., title="Sum of p_reward")
-    RewardFamily: int = Field(..., title="Reward family")  # Should be explicit here
-    RewardPairsN: int = Field(..., title="Number of pairs")  # Should be explicit here
+    base_reward_sum: float = Field(..., title="Sum of p_reward")
+    reward_family: int = Field(..., title="Reward family")  # Should be explicit here
+    reward_pairs_n: int = Field(..., title="Number of pairs")  # Should be explicit here
 
-    UncoupledReward: str = Field("0.1,0.3,0.7", title="Uncoupled reward")  # For uncoupled tasks only
+    uncoupled_reward: str = Field("0.1,0.3,0.7", title="Uncoupled reward")  # For uncoupled tasks only
 
     # Randomness
-    Randomness: str = Field('Exponential', title="Randomness mode")  # Exponential by default
+    randomness: str = Field('Exponential', title="Randomness mode")  # Exponential by default
 
     # Block length
-    BlockMin: int = Field(..., title="Block length (min)")
-    BlockMax: int = Field(..., title="Block length (max)")
-    BlockBeta: int = Field(..., title="Block length (beta)")
-    BlockMinReward: int = Field(1, title="Minimal rewards in a block to switch")
+    block_min: int = Field(..., title="Block length (min)")
+    block_max: int = Field(..., title="Block length (max)")
+    block_beta: int = Field(..., title="Block length (beta)")
+    block_min_reward: int = Field(1, title="Minimal rewards in a block to switch")
 
     # Delay period
-    DelayMin: float = Field(..., title="Delay period (min) ")
-    DelayMax: float = Field(..., title="Delay period (max) ")
-    DelayBeta: float = Field(..., title="Delay period (beta)")
+    delay_min: float = Field(..., title="Delay period (min) ")
+    delay_max: float = Field(..., title="Delay period (max) ")
+    delay_beta: float = Field(..., title="Delay period (beta)")
 
     # Reward delay
-    RewardDelay: float = Field(..., title="Reward delay (sec)")
+    reward_delay: float = Field(..., title="Reward delay (sec)")
 
     # Auto water
-    AutoReward: bool = Field(..., title="Auto reward switch")
-    AutoWaterType: AutoWaterMode = Field(AutoWaterMode.NATURAL, title="Auto water mode")
-    Multiplier: float = Field(..., title="Multiplier for auto reward")
-    Unrewarded: int = Field(..., title="Number of unrewarded trials before auto water")
-    Ignored: int = Field(..., title="Number of ignored trials before auto water")
+    auto_reward: bool = Field(..., title="Auto reward switch")
+    auto_water_type: AutoWaterMode = Field(AutoWaterMode.NATURAL, title="Auto water mode")
+    multiplier: float = Field(..., title="Multiplier for auto reward")
+    unrewarded: int = Field(..., title="Number of unrewarded trials before auto water")
+    ignored: int = Field(..., title="Number of ignored trials before auto water")
 
     # ITI
-    ITIMin: float = Field(..., title="ITI (min)")
-    ITIMax: float = Field(..., title="ITI (max)")
-    ITIBeta: float = Field(..., title="ITI (beta)")
-    ITIIncrease: float = Field(0.0, title="ITI increase")  # TODO: not implemented in the GUI??
+    iti_min: float = Field(..., title="ITI (min)")
+    iti_max: float = Field(..., title="ITI (max)")
+    iti_beta: float = Field(..., title="ITI (beta)")
+    iti_increase: float = Field(0.0, title="ITI increase")  # TODO: not implemented in the GUI??
 
     # Response time
-    ResponseTime: float = Field(..., title="Response time")
-    RewardConsumeTime: float = Field(..., title="Reward consume time",
+    response_time: float = Field(..., title="Response time")
+    reward_consume_time: float = Field(..., title="Reward consume time",
                                      description="Time of the no-lick period before trial end")
-    StopIgnores: int = Field(..., title="Number of ignored trials before stop")
+    stop_ignores: int = Field(..., title="Number of ignored trials before stop")
 
     # Auto block
-    AdvancedBlockAuto: AdvancedBlockMode = Field(..., title="Auto block mode")
-    SwitchThr: float = Field(..., title="Switch threshold for auto block")
-    PointsInARow: int = Field(..., title="Points in a row for auto block")
+    advanced_block_auto: AdvancedBlockMode = Field(..., title="Auto block mode")
+    switch_thr: float = Field(..., title="Switch threshold for auto block")
+    points_in_a_row: int = Field(..., title="Points in a row for auto block")
 
     # Auto stop
-    MaxTrial: int = Field(..., title="Maximal number of trials")
-    MaxTime: int = Field(..., title="Maximal session time (min)")
+    max_trial: int = Field(..., title="Maximal number of trials")
+    max_time: int = Field(..., title="Maximal session time (min)")
 
     # Reward size
-    RightValue_volume: float = Field(3.00, title="Right reward size (uL)")
-    LeftValue_volume: float = Field(3.00, title="Left reward size (uL)")
+    right_value_volume: float = Field(3.00, title="Right reward size (uL)")
+    left_value_volume: float = Field(3.00, title="Left reward size (uL)")
 
     # Warmup
     warmup: str = Field('off', title="Warmup master switch")
