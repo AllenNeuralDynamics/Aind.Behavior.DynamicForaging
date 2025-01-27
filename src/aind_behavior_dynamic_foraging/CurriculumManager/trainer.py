7c5f60bf0ea3ecba5245a54b3d671ea09f0e9e1e
Micah Woodard <micah.woodard@alleninstitute.org>
2025-01-27 11:49:46 -0800

    adding check for slims

--- src/aind_behavior_dynamic_foraging/CurriculumManager/trainer.py
+++ src/aind_behavior_dynamic_foraging/CurriculumManager/trainer.py
@@ -4,7 +4,7 @@ from aind_behavior_curriculum import (
     TrainerServer,
     TrainerState,
 )
-from aind_slims_api import SlimsClient, models
+import aind_slims_api as slims
 import logging
 import os
 from aind_data_access_api.document_db import MetadataDbClient
@@ -13,7 +13,7 @@ from datetime import datetime
 
 
 class DynamicForagingTrainerServer(TrainerServer):
-    def __init__(self, slims_client: SlimsClient = None, docdb_client: MetadataDbClient = None) -> None:
+    def __init__(self, slims_client: slims.SlimsClient = None, docdb_client: MetadataDbClient = None) -> None:
         """
         Dynamic Foraging Trainer that loads data from Slims and DocDB and writes trainer state to Slims
 
@@ -24,6 +24,7 @@ class DynamicForagingTrainerServer(TrainerServer):
         self.log = logging.getLogger(f"{__name__}.{self.__class__.__name__}")
 
         self.slims_client = slims_client if slims_client else self._connect_to_slims()
+        self._check_slims_access()
 
         self.docdb_client = docdb_client if docdb_client else MetadataDbClient(
             host='api.allenneuraldynamics.org',
@@ -31,7 +32,7 @@ class DynamicForagingTrainerServer(TrainerServer):
             collection='data_assets'
         )
 
-    def _connect_to_slims(self) -> SlimsClient:
+    def _connect_to_slims(self) -> slims.SlimsClient:
         """
         Connect to Slims client
 
@@ -40,26 +41,33 @@ class DynamicForagingTrainerServer(TrainerServer):
 
         try:
             self.log.info('Attempting to connect to Slims')
-            slims_client = SlimsClient(username=os.environ['SLIMS_USERNAME'],
+            slims_client = slims.SlimsClient(username=os.environ['SLIMS_USERNAME'],
                                        password=os.environ['SLIMS_PASSWORD'])
         except KeyError as e:
             raise KeyError('SLIMS_USERNAME and SLIMS_PASSWORD do not exist as '
                            f'environment variables on machine. Please add. {e}')
 
+        self.log.info('Successfully connected to Slims')
+
+        return slims_client
+
+    def _check_slims_access(self) -> None:
+        """
+        Method to check ability to read from Slims. Trying to catch instances of incorrect credentials
+        """
+
         try:
-            slims_client.fetch_model(models.SlimsMouseContent, barcode='00000000')
+            self.slims_client.fetch_model(slims.models.SlimsMouseContent, barcode='00000000')
         except Exception as e:
             if 'Status 401 – Unauthorized' in str(e):  # catch error if username and password are incorrect
                 raise Exception(f'Exception trying to read from Slims: {e}.\n'
-                                f' Please check credentials:\n'
+                                f'Please check credentials:\n'
                                 f'Username: {os.environ["SLIMS_USERNAME"]}\n'
                                 f'Password: {os.environ["SLIMS_PASSWORD"]}')
             elif 'No record found' not in str(e):  # bypass if mouse doesn't exist
                 raise Exception(f'Exception trying to read from Slims: {e}.\n')
 
-        self.log.info('Successfully connected to Slims')
-
-        return slims_client
+        self.log.debug('Successfully read from Slims.')
 
     def load_data(self, subject_id: str) -> tuple[Curriculum, TrainerState, Metrics]:
         """
@@ -70,8 +78,8 @@ class DynamicForagingTrainerServer(TrainerServer):
         """
 
         # grab trainer state from slims
-        mouse = self.slims_client.fetch_model(models.SlimsMouseContent, barcode=subject_id)
-        last_session = self.slims_client.fetch_models(models.behavior_session.SlimsBehaviorSession,
+        mouse = self.slims_client.fetch_model(slims.models.SlimsMouseContent, barcode=subject_id)
+        last_session = self.slims_client.fetch_models(slims.models.behavior_session.SlimsBehaviorSession,
                                                       mouse_pk=mouse.pk, start=0, end=1)
         curriculum_attachments = self.slims_client.fetch_attachments(last_session[0])
         response = self.slims_client.fetch_attachment_content(curriculum_attachments[0]).json()
@@ -98,7 +106,8 @@ class DynamicForagingTrainerServer(TrainerServer):
         foraging_efficiency = [epoch['output_parameters']['performance']['foraging_efficiency'] for epoch in epochs]
 
         # query slims for sessions to determine how many sessions at current stage
-        slims_session = self.slims_client.fetch_models(models.behavior_session.SlimsBehaviorSession, mouse_pk=mouse.pk)
+        slims_session = self.slims_client.fetch_models(slims.models.behavior_session.SlimsBehaviorSession,
+                                                       mouse_pk=mouse.pk)
         current_stage = trainer_state.stage.name
         session_at_current_stage = len([session for session in slims_session if session.task_stage == current_stage])
 
@@ -126,11 +135,11 @@ class DynamicForagingTrainerServer(TrainerServer):
         :experimenters: list of experimenters who ran session
         """
 
-        mouse = self.slims_client.fetch_model(models.SlimsMouseContent, barcode=subject_id)
+        mouse = self.slims_client.fetch_model(slims.models.SlimsMouseContent, barcode=subject_id)
 
         # add session to slims
         added_session = self.slims_client.add_model(
-            models.SlimsBehaviorSession(
+            slims.models.SlimsBehaviorSession(
                 mouse_pk=mouse.pk,
                 task_stage=trainer_state.stage.name,
                 task=curriculum.name,
--- tests/test_trainer_server.py
+++ tests/test_trainer_server.py
@@ -3,7 +3,9 @@ from aind_behavior_dynamic_foraging import DynamicForagingTrainerServer, Dynamic
 from tests.mock_databases import MockSlimsClient, MockDocDBClient
 from aind_behavior_dynamic_foraging.CurriculumManager.curriculums.coupled_baiting_2p3 import \
     construct_coupled_baiting_2p3_curriculum
-
+from unittest import mock
+import os
+from slims.internal import Record
 
 class TestTrainerServer(unittest.TestCase):
     """ Testing TrainerServer model"""
@@ -20,6 +22,36 @@ class TestTrainerServer(unittest.TestCase):
 
         self.trainer_server = DynamicForagingTrainerServer(slims_client=self.slims_client,
                                                            docdb_client=self.docdb_client)
+
+    @mock.patch.dict(os.environ, {"SLIMS_USERNAME": "user",
+                                  "SLIMS_PASSWORD": "lemme_in"})
+    def test_internal_slims_client(self):
+        """
+        Test creating of slims client within class
+        """
+
+        # mock slims client for success
+        with mock.patch('aind_slims_api.SlimsClient', autospec=True) as MockSlimsClient:
+            trainer_server = DynamicForagingTrainerServer(docdb_client=self.docdb_client)
+
+        # mock for failed
+        with self.assertRaises(Exception) as context:
+            DynamicForagingTrainerServer(docdb_client=self.docdb_client)
+
+        expected_exception = 'Exception trying to read from Slims: ' \
+                             'Could not fetch entities: <!doctype html><html lang="en"><head><title>HTTP ' \
+                             'Status 401 – Unauthorized</title><style type="text/css">body ' \
+                             '{font-family:Tahoma,Arial,sans-serif;} h1, h2, h3, b ' \
+                             '{color:white;background-color:#525D76;} h1 {font-size:22px;} h2 {font-size:16px;} ' \
+                             'h3 {font-size:14px;} p {font-size:12px;} a {color:black;} .line ' \
+                             '{height:1px;background-color:#525D76;border:none;}</style></head><body><h1>' \
+                             'HTTP Status 401 – Unauthorized</h1></body></html>.\n' \
+                             'Please check credentials:\n' \
+                             'Username: user\n' \
+                             'Password: lemme_in'
+
+        self.assertEqual(expected_exception, str(context.exception))
+
 
     def test_load_data(self):
         """
