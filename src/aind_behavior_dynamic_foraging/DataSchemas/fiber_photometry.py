18e8d14c8208fc9a88972661c4cdd78cbca2951f
Micah Woodard <micah.woodard@alleninstitute.org>
2025-02-11 11:48:56 -0800

    changing fib to better populate widget

--- src/aind_behavior_dynamic_foraging/DataSchemas/fiber_photometry.py
+++ src/aind_behavior_dynamic_foraging/DataSchemas/fiber_photometry.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel, Field, NonNegativeFloat, RootModel, model_validator
 from typing import Annotated, Dict, List, Literal, Optional, Self, Union
 
+MODES = Literal["Normal", "Axon"]
 
 class FiberPhotometry(BaseModel):
     experiment_type: Literal["FiberPhotometry"] = "FiberPhotometry"
-    mode: Literal["Normal", "Axon"] = Field(default="Normal", description="FIP mode for experiment.")
+    mode: MODES = Field(default="Normal", description="FIP mode for experiment.")
     baseline_time: int = Field(default=10, description="Baseline time before experiment starts in minutes.")
