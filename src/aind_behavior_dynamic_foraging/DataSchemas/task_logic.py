7ecfa89046aa6f2a44f674a59d56cc5b8411e723
Micah Woodard <micah.woodard@alleninstitute.org>
2025-02-27 09:42:31 -0800

    fixing defaults

--- src/aind_behavior_dynamic_foraging/DataSchemas/task_logic.py
+++ src/aind_behavior_dynamic_foraging/DataSchemas/task_logic.py
@@ -27,9 +27,9 @@ class RewardProbability(BaseModel):
 
 
 class DelayPeriod(BaseModel):
-    min: float = Field(default=0, title="Delay period (min) ")
-    max: float = Field(default=1, title="Delay period (max) ")
-    beta: float = Field(default=1, title="Delay period (beta)")
+    min: float = Field(default=0.0, title="Delay period (min) ")
+    max: float = Field(default=1.0, title="Delay period (max) ")
+    beta: float = Field(default=1.0, title="Delay period (beta)")
 
 
 class AutoWaterMode(str, Enum):
@@ -48,15 +48,15 @@ class AutoWater(BaseModel):
 
 
 class InterTrialInterval(BaseModel):
-    min: float = Field(default=1, title="ITI (min)")
-    max: float = Field(default=8, title="ITI (max)")
-    beta: float = Field(default=2, title="ITI (beta)")
+    min: float = Field(default=1.0, title="ITI (min)")
+    max: float = Field(default=8.0, title="ITI (max)")
+    beta: float = Field(default=2.0, title="ITI (beta)")
     increase: float = Field(default=0.0, title="ITI increase")  # TODO: not implemented in the GUI??
 
 
 class ResponseTime(BaseModel):
-    response_time: float = Field(default=1, title="Response time")
-    reward_consume_time: float = Field(default=3, title="Reward consume time",
+    response_time: float = Field(default=1.0, title="Response time")
+    reward_consume_time: float = Field(default=3.0, title="Reward consume time",
                                        description="Time of the no-lick period before trial end")
 
 
