b5ec52e93f5252ef6f295219720b4de6e095fe8c
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2026-01-31 20:30:01 -0800

    Improve Trial definition

--- src/aind_behavior_dynamic_foraging/task_logic.py
+++ src/aind_behavior_dynamic_foraging/task_logic.py
@@ -1,5 +1,4 @@
-import logging
-from typing import Literal, Optional
+from typing import TYPE_CHECKING, Annotated, Literal, Optional, TypeAliasType, Union
 
 from aind_behavior_services.task import Task, TaskParameters
 from pydantic import BaseModel, Field
@@ -7,8 +6,6 @@ from pydantic import BaseModel, Field
 from aind_behavior_dynamic_foraging import (
     __semver__,
 )
-
-logger = logging.getLogger(__name__)
 
 RANDOMNESSES = Literal["Exponential", "Even"]
 AUTO_WATER_MODES = Literal["Natural", "Both", "High pro"]
@@ -138,22 +135,76 @@ class AindDynamicForagingTaskLogic(Task):
     )
     task_parameters: AindDynamicForagingTaskParameters = Field(description="Parameters of the task logic")
 
+
+class AuditorySecondaryReinforcer(BaseModel):
+    """Represents an auditory secondary reinforcer."""
+
+    type: Literal["Auditory"] = "Auditory"
+    frequency_or_index: int = Field(
+        default=15000,
+        title="Frequency or index of the auditory secondary reinforcer. Indices correspond to < 32 values",
+    )
+
+
+if TYPE_CHECKING:
+    SecondaryReinforcer = Union[(AuditorySecondaryReinforcer,)]
+else:
+    SecondaryReinforcer = TypeAliasType(
+        "SecondaryReinforcer",
+        Annotated[
+            Union[(AuditorySecondaryReinforcer,)],
+            Field(discriminator="type", description="Type of secondary reinforcer"),
+        ],
+    )
+
+
 class Trial(BaseModel):
-    has_secondary_reinforcer: bool
-    has_reward_left: bool
-    has_reward_right: bool
-    quiescent_period_wait_duration: float
-    choice_deadline: float
-    right_is_active: bool
-    left_is_active: bool
-    is_fast_retract: bool
-    reward_consumption_duration: float
-    iti_duration: float
-    reward_delay: float
-    is_auto_response_right: Optional[bool]
-    lickspout_offset: float
+    """Represents a single trial that can be instantiated by the Bonsai state machine."""
+
+    has_reward_left: bool = Field(
+        default=True, description="Indicates if there is a reward on the left side if response is made."
+    )
+    has_reward_right: bool = Field(
+        default=True, description="Indicates if there is a reward on the right side if response is made."
+    )
+    reward_consumption_duration: float = Field(
+        default=5.0, ge=0, description="Duration of reward consumption before transition to ITI (in seconds)."
+    )
+    reward_delay_duration: float = Field(
+        default=0.0, ge=0, description="Delay before reward is delivered after the secondary reinforcer (in seconds)."
+    )
+    secondary_reinforcer: Optional[SecondaryReinforcer] = Field(
+        default=None, description="Defines the secondary reinforcer used in the trial."
+    )
+    response_deadline_duration: float = Field(
+        default=5.0, ge=0, description="Time allowed for the subject to make a response (in seconds)."
+    )
+    enable_fast_retract: bool = Field(
+        default=False, description="If true, the opposite lickspout retracts quickly after a response is made."
+    )
+    quiescence_period_duration: float = Field(
+        default=0.5,
+        ge=0,
+        description="Duration of the quiescence period before trial starts (in seconds). Each lick resets the timer.",
+    )
+    inter_trial_interval_duration: float = Field(
+        default=5.0, ge=0, description="Duration of the inter-trial interval (in seconds)."
+    )
+    is_auto_response_right: Optional[bool] = Field(
+        default=None,
+        description="If set, the trial will automatically (and immediately) register a response to the right (True) or left (False).",
+    )
+    lickspout_offset: float = Field(
+        default=0.0,
+        description="Horizontal offset of the lickspouts (in mm). Positive values move the lickspouts right.",
+    )
+
 
 class TrialOutcome(BaseModel):
-    trial: Trial
-    choice: Optional[bool]
-    is_rewarded: bool
+    """Represents the outcome of a single trial."""
+
+    trial: Trial = Field(description="The trial associated with this outcome.")
+    is_right_choice: Optional[bool] = Field(
+        description="Reports the choice made by the subject. True for right, False for left, None for no choice."
+    )
+    is_rewarded: bool = Field(description="Indicates whether the subject received a reward on this trial.")
