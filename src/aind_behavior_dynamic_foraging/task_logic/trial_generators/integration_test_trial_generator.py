58614c5b5e09743b7676943e234c15c6c0d8e93e
Micah Woodard <micah.woodard@alleninstitute.org>
2026-03-06 15:35:30 -0800

    shortens integration

--- src/aind_behavior_dynamic_foraging/task_logic/trial_generators/integration_test_trial_generator.py
+++ src/aind_behavior_dynamic_foraging/task_logic/trial_generators/integration_test_trial_generator.py
@@ -19,49 +19,49 @@ class IntegrationTestTrialGenerator(ITrialGenerator):
         self.trial_opts = [
             Trial(),  # 0: left and right reward
             Trial(p_reward_left=1.0, p_reward_right=0.0),  # 1: left reward
-            Trial(p_reward_left=0.0, p_reward_right=1.0),  # 2: right reward
-            Trial(p_reward_left=0.0, p_reward_right=0.0),  # 3: no reward
-            Trial(p_reward_left=1.0, p_reward_right=1.0),  # 4: both reward
-            # auto response right
-            Trial(
-                p_reward_left=1.0, p_reward_right=0.0, is_auto_response_right=True
-            ),  # 5: left reward, auto response right
-            Trial(
-                p_reward_left=0.0, p_reward_right=1.0, is_auto_response_right=True
-            ),  # 6: right reward, auto response right
-            Trial(
-                p_reward_left=1.0, p_reward_right=1.0, is_auto_response_right=True
-            ),  # 7: both reward, auto response right,
-            Trial(
-                p_reward_left=0.0, p_reward_right=0.0, is_auto_response_right=True
-            ),  # 8: no reward, auto response right,
-            # auto response left
-            Trial(
-                p_reward_left=1.0, p_reward_right=0.0, is_auto_response_right=False
-            ),  # 9: left reward, auto response left,
-            Trial(
-                p_reward_left=0.0, p_reward_right=1.0, is_auto_response_right=False
-            ),  # 10: right reward, auto response left,
-            Trial(
-                p_reward_left=0.0, p_reward_right=1.0, is_auto_response_right=False
-            ),  # 11: both reward, auto response left,
-            Trial(
-                p_reward_left=0.0, p_reward_right=0.0, is_auto_response_right=False
-            ),  # 12: no reward, auto response left,
-            # fast retract
-            Trial(enable_fast_retract=True),  # 13: enable fast retract
-            # secondary reinforcer
-            # Trial(secondary_reinforcer=SecondaryReinforcer()),   # 14: enable secondary reinforcer
-            # no reward consumption duration
-            Trial(reward_consumption_duration=0),  # 15: no reward consumption duration
-            # no reward delay
-            Trial(reward_delay_duration=0),  # 16: no reward delay duration
-            # no response deadline duration
-            Trial(response_deadline_duration=0),  # 17: no response deadline duration
-            # no quiescence period duration
-            Trial(quiescence_period_duration=0),  # 18: no quiescence period duration
-            # no inter trial interval
-            Trial(inter_trial_interval_duration=0.5),  # 19: no inter trial interval duration
+            # Trial(p_reward_left=0.0, p_reward_right=1.0),  # 2: right reward
+            # Trial(p_reward_left=0.0, p_reward_right=0.0),  # 3: no reward
+            # Trial(p_reward_left=1.0, p_reward_right=1.0),  # 4: both reward
+            # # auto response right
+            # Trial(
+            #     p_reward_left=1.0, p_reward_right=0.0, is_auto_response_right=True
+            # ),  # 5: left reward, auto response right
+            # Trial(
+            #     p_reward_left=0.0, p_reward_right=1.0, is_auto_response_right=True
+            # ),  # 6: right reward, auto response right
+            # Trial(
+            #     p_reward_left=1.0, p_reward_right=1.0, is_auto_response_right=True
+            # ),  # 7: both reward, auto response right,
+            # Trial(
+            #     p_reward_left=0.0, p_reward_right=0.0, is_auto_response_right=True
+            # ),  # 8: no reward, auto response right,
+            # # auto response left
+            # Trial(
+            #     p_reward_left=1.0, p_reward_right=0.0, is_auto_response_right=False
+            # ),  # 9: left reward, auto response left,
+            # Trial(
+            #     p_reward_left=0.0, p_reward_right=1.0, is_auto_response_right=False
+            # ),  # 10: right reward, auto response left,
+            # Trial(
+            #     p_reward_left=0.0, p_reward_right=1.0, is_auto_response_right=False
+            # ),  # 11: both reward, auto response left,
+            # Trial(
+            #     p_reward_left=0.0, p_reward_right=0.0, is_auto_response_right=False
+            # ),  # 12: no reward, auto response left,
+            # # fast retract
+            # Trial(enable_fast_retract=True),  # 13: enable fast retract
+            # # secondary reinforcer
+            # # Trial(secondary_reinforcer=SecondaryReinforcer()),   # 14: enable secondary reinforcer
+            # # no reward consumption duration
+            # Trial(reward_consumption_duration=0),  # 15: no reward consumption duration
+            # # no reward delay
+            # Trial(reward_delay_duration=0),  # 16: no reward delay duration
+            # # no response deadline duration
+            # Trial(response_deadline_duration=0),  # 17: no response deadline duration
+            # # no quiescence period duration
+            # Trial(quiescence_period_duration=0),  # 18: no quiescence period duration
+            # # no inter trial interval
+            # Trial(inter_trial_interval_duration=0.5),  # 19: no inter trial interval duration
         ]
 
     def next(self) -> Trial | None:
