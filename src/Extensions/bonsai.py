09e3db3167384c53d59b92c5790436062572ea21
Micah Woodard <micah.woodard@alleninstitute.org>
2026-03-16 09:16:26 -0700

    setsup logging and bumbs aind behavior services

--- src/Extensions/bonsai.py
+++ src/Extensions/bonsai.py
@@ -7,6 +7,25 @@ from aind_behavior_dynamic_foraging.task_logic import TrialGeneratorSpec
 if TYPE_CHECKING:
     from aind_behavior_dynamic_foraging.task_logic.trial_generators._base import ITrialGenerator
 
+import logging
+import logging.config
+
+logging_config = {
+    "version": 1, 
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout"
+        },
+    },
+    "root": {
+        "handlers": ["console"]
+    }
+}
+
+logging.config.dictConfig(logging_config)
+logging.getLogger(__name__)
 
 def resolve_generator(spec: TrialGeneratorSpec | str) -> "ITrialGenerator":
     """Resolves and creates the trial generator instance based on the task logic's trial generator model."""
