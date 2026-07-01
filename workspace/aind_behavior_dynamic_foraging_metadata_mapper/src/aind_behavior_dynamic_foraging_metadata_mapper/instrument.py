e55c0ea6c7883a2cd71faaf4e150c6fdeb0f8806
Micah Woodard <micah.woodard@alleninstitute.org>
2026-07-01 09:41:31 -0700

    undoes version

--- workspace/aind_behavior_dynamic_foraging_metadata_mapper/src/aind_behavior_dynamic_foraging_metadata_mapper/instrument.py
+++ workspace/aind_behavior_dynamic_foraging_metadata_mapper/src/aind_behavior_dynamic_foraging_metadata_mapper/instrument.py
@@ -155,9 +155,8 @@ class AindInstrumentDataMapper(AindDataSchemaRigDataMapper):
                 serial_number=rig.harp_behavior.serial_number,
                 manufacturer=Organization.CHAMPALIMAUD,
                 is_clock_generator=False,
-                firmware_version=behavior_board["FirmwareVersionHigh"],
-                hardware_version=behavior_board["HardwareVersionHigh"],
-                core_version=behavior_board["CoreVersionHigh"],
+                firmware_version=behavior_board.device.firmwareVersion,
+                hardware_version=behavior_board.device.hardwareTargets,
             )
         )
 
@@ -169,9 +168,8 @@ class AindInstrumentDataMapper(AindDataSchemaRigDataMapper):
                 harp_device_type=HarpDeviceType.WHITERABBIT,
                 serial_number=rig.harp_clock_generator.serial_number,
                 is_clock_generator=True,
-                firmware_version=clock_generator["FirmwareVersionHigh"],
-                hardware_version=clock_generator["HardwareVersionHigh"],
-                core_version=clock_generator["CoreVersionHigh"],
+                firmware_version=clock_generator.device.firmwareVersion,
+                hardware_version=clock_generator.device.hardwareTargets,
             )
         )
 
