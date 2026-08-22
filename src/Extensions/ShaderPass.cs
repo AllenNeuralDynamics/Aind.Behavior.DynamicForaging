495f781cdd2a27ca50ceed901d1e71f1f7614613
RoboDoig <erskine94@gmail.com>
2026-08-22 11:49:06 +0100

    Working basic shader pass

--- src/Extensions/ShaderPass.cs
+++ src/Extensions/ShaderPass.cs
@@ -20,7 +20,7 @@ public class ShaderPass : Combinator<ImTextureRef, ImTextureRef>
 
     void main()
     {
-        texCoord = vec2(.5, -.5) * (vertexPosition.xy + 1);
+        texCoord = vec2(.5, .5) * (vertexPosition.xy + 1);
         gl_Position = vec4(vertexPosition.xy, 0.0, 1.0);
     }
     ";
@@ -38,31 +38,103 @@ public class ShaderPass : Combinator<ImTextureRef, ImTextureRef>
         }
         ";
 
+    static readonly float[] QuadVertices = new float[] { -1f, -1f, 1f, -1f, -1f, 1f, 1f, 1f };
+
     public override IObservable<ImTextureRef> Process(IObservable<ImTextureRef> source)
     {
         return Observable.Defer(() =>
         {
             int shaderProgram = 0;
+            int vertexArray = 0;
+            int framebuffer = 0;
+            int targetTexture = 0;
+            int targetWidth = 0;
+            int targetHeight = 0;
+            var targetRef = default(ImTextureRef);
             return source.Select(texture =>
             {
                 var currentContext = ImGui.GetCurrentContext();
                 var sourceTexture = (int)(ulong)texture.GetTexID();
+                if (sourceTexture == 0) return texture;
 
                 if (shaderProgram == 0)
                 {
                     shaderProgram = CreateProgram(VertexShaderSource, DefaultFragmentShader);
+                    vertexArray = CreateQuad();
+                    framebuffer = GL.GenFramebuffer();
                 }
 
+                // The pass renders into a texture of the same size as the incoming one.
+                int width, height;
+                GL.BindTexture(TextureTarget.Texture2D, sourceTexture);
+                GL.GetTexLevelParameter(TextureTarget.Texture2D, 0, GetTextureParameter.TextureWidth, out width);
+                GL.GetTexLevelParameter(TextureTarget.Texture2D, 0, GetTextureParameter.TextureHeight, out height);
+                if (width != targetWidth || height != targetHeight)
+                {
+                    targetTexture = CreateTarget(targetTexture, width, height);
+                    targetRef = CreateTextureRef(targetTexture);
+                    targetWidth = width;
+                    targetHeight = height;
+                    GL.BindFramebuffer(FramebufferTarget.Framebuffer, framebuffer);
+                    GL.FramebufferTexture2D(
+                        FramebufferTarget.Framebuffer, FramebufferAttachment.ColorAttachment0,
+                        TextureTarget.Texture2D, targetTexture, 0);
+                    GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
+                }
+
+                var viewport = new int[4];
+                GL.GetInteger(GetPName.Viewport, viewport);
+                GL.BindFramebuffer(FramebufferTarget.Framebuffer, framebuffer);
+                GL.Viewport(0, 0, width, height);
                 GL.UseProgram(shaderProgram);
 
                 GL.ActiveTexture(TextureUnit.Texture0);
                 GL.BindTexture(TextureTarget.Texture2D, sourceTexture);
 
-                GL.UseProgram(0);
+                GL.BindVertexArray(vertexArray);
+                GL.DrawArrays(PrimitiveType.TriangleStrip, 0, 4);
 
-                return texture;
+                GL.BindVertexArray(0);
+                GL.UseProgram(0);
+                GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
+                GL.Viewport(viewport[0], viewport[1], viewport[2], viewport[3]);
+
+                return targetRef;
             });
         });
+    }
+
+    static int CreateQuad()
+    {
+        var vertexArray = GL.GenVertexArray();
+        GL.BindVertexArray(vertexArray);
+        GL.BindBuffer(BufferTarget.ArrayBuffer, GL.GenBuffer());
+        GL.BufferData(
+            BufferTarget.ArrayBuffer, QuadVertices.Length * sizeof(float),
+            QuadVertices, BufferUsageHint.StaticDraw);
+        GL.EnableVertexAttribArray(0);
+        GL.VertexAttribPointer(0, 2, VertexAttribPointerType.Float, false, 0, 0);
+        GL.BindVertexArray(0);
+        return vertexArray;
+    }
+
+    static int CreateTarget(int texture, int width, int height)
+    {
+        if (texture == 0) texture = GL.GenTexture();
+        GL.BindTexture(TextureTarget.Texture2D, texture);
+        GL.TexImage2D(
+            TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, width, height, 0,
+            PixelFormat.Rgba, PixelType.UnsignedByte, IntPtr.Zero);
+        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+        GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+        return texture;
+    }
+
+    static ImTextureRef CreateTextureRef(int texture)
+    {
+        var textureRef = default(ImTextureRef);
+        textureRef.TexID = new ImTextureID((IntPtr)texture);
+        return textureRef;
     }
 
     static int CreateProgram(string vertexCode, string fragmentCode)
@@ -83,7 +155,6 @@ public class ShaderPass : Combinator<ImTextureRef, ImTextureRef>
         {
             var infoLog = GL.GetProgramInfoLog(program);
             GL.DeleteProgram(program);
-            Console.WriteLine(infoLog);
             throw new InvalidOperationException(string.Format("Failed to link the shader program: {0}", infoLog));
         }
 
