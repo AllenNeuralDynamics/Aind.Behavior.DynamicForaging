1d2cdef4b5872b97f92df0299c09908d628dff17
Micah Woodard <micah.woodard@alleninstitute.org>
2026-09-03 10:11:42 -0700

    evaluates is_rewarded to false if animal does not choose auto rewarded side

--- src/Extensions/DetermineReward.cs
+++ src/Extensions/DetermineReward.cs
@@ -23,7 +23,7 @@ public class DetermineReward
         {
             var response = value.Item1;
             var trial = value.Item2;
-            if (!response.HasValue)
+            if (!response.HasValue || trial.IsAutoRewardRight != response.Value)
             {
                 return false;
             }
